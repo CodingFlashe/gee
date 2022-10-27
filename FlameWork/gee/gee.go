327049682ec1e5ad8a1ef3fa0a9b230e166e07f3
CodingFlashe <3531804187@qq.com>
2022-10-28 01:08:13 +0800

    day3-finish

--- FlameWork/gee/gee.go
+++ FlameWork/gee/gee.go
@@ -1,6 +1,7 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -10,27 +11,55 @@ type HandlerFunc func(c *Context)
 // Engine 用于实现接口ServeHTTP
 // router 是路由映射表
 // key 由请求方法和静态路由地址构成, value 是用户映射的处理方法
-type Engine struct {
-	router *router
-}
+type (
+	RouterGroup struct {
+		prefix      string
+		middlewares []HandlerFunc //支持中间件
+		parent      *RouterGroup  //支持嵌套
+		engine      *Engine       //所有分组共享同一个engine实例
+	}
+	Engine struct {
+		*RouterGroup
+		router *router
+		groups []*RouterGroup
+	}
+)
 
 // New 是gee.Engine的构造器
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	engine.router.addRoute(method, pattern, handler)
+// Group 被定义用来产生一个新的分组
+// 记住所有的组都共享相同的engine实例
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
 }
 
-// Get 定义添加GET请求的方法
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+// GET 定义添加GET请求的方法
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
 }
 
 // POST 定义添加POST请求的方法
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 // Run 定义启动http服务的方法
